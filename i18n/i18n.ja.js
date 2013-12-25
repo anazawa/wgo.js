88d3599981f8490488cc74c8f7ae362dc184f234
Ryo Anazawa <anazawa@cpan.org>
2013-12-25 18:21:50 +0900

    add Japanese translation

---
+++ i18n/i18n.ja.js
@@ -0,0 +1,57 @@
+// This file serves as template for translations
+// You can just replace english strings with translated strings.
+
+WGo.lang = "ja"; // set language code
+
+WGo.i18n[WGo.lang] = {
+	"about-text": "<h1>WGo.js Player 2.0</h1>"
+				+ "<p>WGo.js Player is extension of WGo.js, HTML5 library for purposes of game of go. It allows to replay go game records and it has many features like score counting. It is also designed to be easily extendable.</p>"
+				+ "<p>WGo.js is open source licensed under <a href='http://en.wikipedia.org/wiki/MIT_License' target='_blank'>MIT license</a>. You can use and modify any code from this project.</p>"
+				+ "<p>You can find more information at <a href='http://wgo.waltheri.net/player' target='_blank'>wgo.waltheri.net/player</a></p>"
+				+ "<p>Copyright &copy; 2013 Jan Prokop</p>",
+	"black": "黒",
+	"white": "白",
+	"DT": "対局日",
+	"KM": "コミ",
+	"HA": "置き石",
+	"AN": "注釈",
+	"CP": "著作権",
+	"OT": "Overtime",
+	"TM": "持ち時間",
+	"RE": "Result",
+	"RU": "ルール",
+	"PC": "会場",
+	"EV": "Event",
+	"SO": "Source",
+	"none": "none",
+	"minute": "分",
+	"minutes": "分",
+	"second": "秒",
+	"seconds": "秒",
+	"bpass": "Black passed.",
+	"wpass": "White passed.",
+	"show": "show",
+	"res-show-tip": "Click to show result.",
+	"BP:closemsg": "click anywhere to close this window",
+	"rank": "段級位",
+	"caps": "アゲハマ",
+	"time": "持ち時間",
+	"about": "About",
+	"first": "First",
+	"multiprev": "十手前へ戻る",
+	"previous": "戻る",
+	"next": "進む",
+	"multinext": "十手後へ進む",
+	"last": "Last",
+	"switch-coo": "座標を表示する",
+	"menu": "Menu",
+	"comments": "Comments",
+	"gameinfo": "Game info",
+	"editmode": "Edit mode",
+	"permalink": "パーマリンク",
+	"scoremode": "Count score",
+	"score": "Score",
+	"bwin": "黒の $ 目勝ち",
+	"wwin": "白の $ 目勝ち",
+	"help_score": "Click on stones to mark them dead or alive. You can also set and unset territory points by clicking on them. Territories must be completely bordered.",
+}
